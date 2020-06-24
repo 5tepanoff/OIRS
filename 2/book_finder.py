c136d8f0384206de8d5358772a88c79032212e03
Stepan.i5 <7tepanis@gmail.com>
2020-06-24 10:23:32 +0300

    Add files via upload

---
+++ 2/book_finder.py
@@ -0,0 +1,44 @@
+import imutils
+import cv2
+
+image = cv2.imread("shapes.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imwrite("gray.jpg", gray)
+
+thresh = cv2.threshold(gray, 20, 255, cv2.THRESH_BINARY)[1]
+
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+for c in cnts:
+	M = cv2.moments(c)
+	cX = int((M["m10"] / M["m00"]))
+	cY = int((M["m01"] / M["m00"]))
+
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.03 * peri, True)
+
+	if len(approx) == 3:
+		shape = "triangle"
+
+	elif len(approx) == 4:
+		(x, y, w, h) = cv2.boundingRect(approx)
+		ar = w / float(h)
+		shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+
+	elif len(approx) == 5:
+		shape = "pentagon"
+
+	elif len(approx) == 6:
+		shape = "hexagon"
+
+	else:
+		shape = "circle"
+
+	c = c.astype("float")
+	c = c.astype("int")
+	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+	cv2.putText(image, shape, (cX-25, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+	cv2.imshow("Image", image)
+	cv2.waitKey(0)
